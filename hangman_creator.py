7aeb4753e1686592059eaf6900312cfd2a3faa26
prateeekgoel7248 <75659121+prateeekgoel7248@users.noreply.github.com>
2021-09-01 15:47:18 +0530

    Add files via upload
    This is my hangman game.

---
+++ hangman_creator.py
@@ -0,0 +1,17 @@
+import hangman_project
+
+
+hangman = hangman_project.Hangman('E:\\my dell\\Desktop\\pgdirectory\\hangman_wordlist.txt')
+hangman.choose_the_word()
+hangman.fill_the_word_status()
+while True:
+    hangman.get_word_status()
+    hangman.guess_the_letter()
+
+    if hangman.attempts_remaining == 0:
+        print("Out of attempts. The word was {}. Game over!".format(hangman.chosen_word))
+        break
+
+    elif hangman.chosen_word == ''.join(hangman.word_status):
+        print("Hurray! You won the game!")
+        break
